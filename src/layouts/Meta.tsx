f1f25accbb37e2d1be98aed9ece8907d6001db4a
soohyun4747 <soohyun4747@gmail.com>
2025-06-09 11:06:12 +0900

    icon and images added

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { NextSeo } from 'next-seo';
 import { AppConfig } from '@/configs/AppConfig';
+import { useRouter } from 'next/router';
 
 type IMetaProps = {
   title?: string;
@@ -9,6 +10,8 @@ type IMetaProps = {
 };
 
 const Meta = (props: IMetaProps) => {
+  const router = useRouter()
+
   return (
     <>
       <Head>
@@ -21,30 +24,25 @@ const Meta = (props: IMetaProps) => {
           name="naver-site-verification"
           content="dc80639662206eb9dcce6ef3f7f83cd028a9a81e"
         /> */}
-        {/* <link
-          rel="apple-touch-icon"
-          href={`${router.basePath}/apple-touch-icon.png`}
-          key="apple"
-        />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
+          href={`${router.basePath}/favicon-32.png`}
           key="icon32"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
+          href={`${router.basePath}/favicon-16.png`}
           key="icon16"
         />
         <link
           rel="icon"
-          href={`${router.basePath}/favicon.ico`}
+          href={`${router.basePath}/favicon.png`}
           key="favicon"
-        /> */}
+        />
       </Head>
       <NextSeo
         title={props.title ?? AppConfig.title}
@@ -56,13 +54,13 @@ const Meta = (props: IMetaProps) => {
           url: props.canonical,
           siteName: AppConfig.og.site_name,
           locale: AppConfig.locale,
-        //   images: [
-        //     {
-        //       url: `${router.basePath}/sns-image.jpg`,
-        //       width: 1200,
-        //       height: 800,
-        //     },
-        //   ],
+          images: [
+            {
+              url: `${router.basePath}/sns-image.png`,
+              width: 1200,
+              height: 800,
+            },
+          ],
         }}
         additionalMetaTags={[
           {
